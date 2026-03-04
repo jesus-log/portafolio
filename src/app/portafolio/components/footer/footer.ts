5a21df2ea4c370d729c4f6689b0036619fa0e17c
Jesús Logroño <jesus_pedrola7@hotmail.com>
2026-03-05 00:37:23 +0100

    Implemento bucle frases y cambio colores fondo

---
+++ src/app/portafolio/components/footer/footer.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  templateUrl: './footer.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Footer {
+
+  year = new Date().getFullYear();
+
+ }
--- src/app/portafolio/pages/main-page/main-page.ts
+++ src/app/portafolio/pages/main-page/main-page.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
@@ -6,4 +6,31 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './main-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MainPage { }
+export class MainPage implements OnInit {
+
+  loading = signal<boolean>(true);
+  typing = signal<boolean>(true);
+  fraseNumber = signal<number>(0);
+
+  ngOnInit(): void {
+    this.endLoading();
+    this.isTyping();
+  }
+
+  endLoading() {
+    setTimeout(() => {
+      this.loading.set(false);
+    }, 4000)
+  }
+
+  isTyping() {
+    setInterval(() => {
+      this.typing.set(!this.typing());
+      this.fraseNumber.set(this.fraseNumber() + 1);
+      if (this.fraseNumber() == 16) this.fraseNumber.set(0);
+    }, 3500);
+  }
+
+  //TODO: Quitar efecto del fondo, implementar los temas,ya tengo fondo/letra, cambiar en la pag principal el tema completo en cada vuelta si no hay un tema seleccionado
+
+}
